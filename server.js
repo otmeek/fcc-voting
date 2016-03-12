75385c52ccbb03cbbe5cf0e0a43c74bafc7a2a4b
meek <otmeek@gmail.com>
2016-03-12 17:05:18 +0000

    responsive layout

--- server.js
+++ server.js
@@ -11,15 +11,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'jade');
 
 app.get('/', function(req, res) {
-    var body = {
-        title: 'FreeCodeCamp basejump Build a Voting App',
-        h1: 'fcc-voting',
-        p1: 'This is a web app project created by otmeek as part of FreeCodeCamp\'s curriculum', 
-        p2: 'Here you can vote on other users\' polls and create your own.'
+    var polls = {
+        data: [
+            {
+                title: 'poll1',
+                url: '#'
+            },
+            {
+                title: 'poll2',
+                url: '#'
+            }
+        ]
     };
-    res.render('index', {
-        body: body
-    });
+    res.render('index', polls);
 });
 
 app.get('/*', function(req, res) {

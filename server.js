db2579dcd2229b1f85e02da3e6578a9e9403e973
meek <otmeek@gmail.com>
2016-03-14 21:42:32 +0000

    single connection to db now

--- server.js
+++ server.js
@@ -9,6 +9,23 @@ var os           = require('os');
 var ifaces       = os.networkInterfaces();
 
 var app = express();
+var db;
+
+var passDb = function(req, res, next) {
+	if (!db) {
+		mongo.connect(process.env.MONGOLAB_URI, function(err, database) {
+			if (err) throw err;
+			db = database;
+
+			req.db = db;
+		  next();
+		});
+	} else {
+	  req.db = db;
+	  next();
+	}
+}
+
 require('dotenv').load();
 
 app.use(express.static(__dirname + '/public'));
@@ -20,23 +37,18 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 app.use(bodyParser.json())
 app.set('view engine', 'jade');
 
-app.get('/', function(req, res) {
+app.get('/', passDb, function(req, res) {
     
     var polls = {};
     
-    mongo.connect(process.env.MONGOLAB_URI, function(err, db) {
-       if(err) throw err; 
-        
-        var collection = db.collection('polls');
-        var data = collection.find().sort({
-            _id: -1
-        }).limit(50).toArray(function(err, docs) {
-            if(err) throw err;
-            polls.data = docs;
-            polls.currentPage = 'Home'
-            res.render('index', polls);
-            db.close();
-        });
+    var collection = req.db.collection('polls');
+    var data = collection.find().sort({
+        _id: -1
+    }).limit(50).toArray(function(err, docs) {
+        if(err) throw err;
+        polls.data = docs;
+        polls.currentPage = 'Home'
+        res.render('index', polls);
     });
 });
 
@@ -47,8 +59,7 @@ app.get('/polls/create', function(req, res) {
     res.render('create', obj);
 });
 
-app.post('/polls/create', function(req, res) {
-    console.log(req.body);
+app.post('/polls/create', passDb, function(req, res) {
     var doc = {
         title: req.body.title,
         totalVotes: 0,
@@ -64,88 +75,76 @@ app.post('/polls/create', function(req, res) {
     }
     
     doc.choices = choices;
-    
-    mongo.connect(process.env.MONGOLAB_URI, function(err, db) {
-        if(err) throw err; 
-        
-        var collection = db.collection('polls');
-        
-        function getNewUrl() {
-            // generate new URL
-            var urlStr = randomstring.generate(10);
-            // check if url already exists
-            collection.find({
-                url: '/polls/' + urlStr
-            }).toArray(function(err, docs) {
-                if(err) throw err;
-                if(docs.length > 0)
-                    getNewUrl();
-            });
-            return urlStr;
-        }
-        
-        doc.url = '/polls/' + getNewUrl();
-        
-        collection.insert(doc, function(err) {
-            if(err) throw err;
-            var obj = {
-                pollSubmitted: true,
-                poll: {
-                    url: process.env.APP_URL + doc.url.substr(1)
-                }
-            }
-            res.render('create', obj);
-            db.close();
-        });
-    });
-    
-    
-});
 
-app.get('/polls/:STRING/vote', function(req, res) {
-    var str = req.params.STRING;
-    
-    mongo.connect(process.env.MONGOLAB_URI, function(err, db) {
-        if(err) throw err;
-        var collection = db.collection('polls');
-        
-        
-        // if user has voted, redirect to results page
-        var ip = [req.headers['x-forwarded-for']];
+    var collection = req.db.collection('polls');
+
+    function getNewUrl() {
+        // generate new URL
+        var urlStr = randomstring.generate(10);
+        // check if url already exists
         collection.find({
-            url: '/polls/' + str,
-            hasVoted: {
-                $in: ip
-            }
+            url: '/polls/' + urlStr
         }).toArray(function(err, docs) {
             if(err) throw err;
-            if(docs.length>0) {
-                // if there is a result, then user has voted
-                console.log('has voted ' + JSON.stringify(docs))
-                // redirect to results page
-                res.redirect('/polls/' + str + '/results');
-                db.close();
-            }
-            else {
-                // user hasnt voted, render vote page
-                console.log('hasnt voted: ' + JSON.stringify(docs));
-                collection.find({
-                    url: /polls/ + str
-                }).toArray(function(err, docs) {
-                    if(err) throw err;
-                    var obj = {
-                        poll: docs[0]
-                    };
-                    res.render('poll', obj);
-                    db.close();
-                });
-            }
+            if(docs.length > 0)
+                getNewUrl();
         });
+        return urlStr;
+    }
+
+    doc.url = '/polls/' + getNewUrl();
+
+    collection.insert(doc, function(err) {
+        if(err) throw err;
+        var obj = {
+            pollSubmitted: true,
+            poll: {
+                url: process.env.APP_URL + doc.url.substr(1)
+            }
+        }
+        res.render('create', obj);
+    });
+    
+    
+});
+
+app.get('/polls/:STRING/vote', passDb, function(req, res) {
+    var str = req.params.STRING;
+    
+
+    var collection = req.db.collection('polls');
+        
+        
+    // if user has voted, redirect to results page
+    var ip = [req.headers['x-forwarded-for']];
+    collection.find({
+        url: '/polls/' + str,
+        hasVoted: {
+            $in: ip
+        }
+    }).toArray(function(err, docs) {
+        if(err) throw err;
+        if(docs.length>0) {
+            // redirect to results page
+            res.redirect('/polls/' + str + '/results');
+        }
+        else {
+            // user hasnt voted, render vote page
+            collection.find({
+                url: /polls/ + str
+            }).toArray(function(err, docs) {
+                if(err) throw err;
+                var obj = {
+                    poll: docs[0]
+                };
+                res.render('poll', obj);
+            });
+        }
     });
 
 });
 
-app.post('/polls/:STRING/vote', function(req, res) {
+app.post('/polls/:STRING/vote', passDb, function(req, res) {
     var str = req.params.STRING;
     var vote = req.body;
     
@@ -154,66 +153,50 @@ app.post('/polls/:STRING/vote', function(req, res) {
     voteObj[key] = 1;
     voteObj.totalVotes = 1;
     
-    mongo.connect(process.env.MONGOLAB_URI, function(err, db) {
-        if (err) throw err;
-        var collection = db.collection('polls');
-        collection.update({
-            url: '/polls/' + str
-        }, {
-            $inc: voteObj,
-            $push: {
-                hasVoted: {
-                    $each: [req.headers['x-forwarded-for']]
-                }
+   
+    var collection = req.db.collection('polls');
+    collection.update({
+        url: '/polls/' + str
+    }, {
+        $inc: voteObj,
+        $push: {
+            hasVoted: {
+                $each: [req.headers['x-forwarded-for']]
             }
-        }, function(err) {
-            if(err) throw err;
-            // redirect to results page
-            res.redirect('/polls/' + str + '/results');
-            db.close();
-        });
+        }
+    }, function(err) {
+        if(err) throw err;
+        // redirect to results page
+        res.redirect('/polls/' + str + '/results');
     });
 
     
 });
 
-app.get('/polls/:STRING/results', function(req, res) {
+app.get('/polls/:STRING/results', passDb, function(req, res) {
     var str = req.params.STRING;
     var obj = {};
-    mongo.connect(process.env.MONGOLAB_URI, function(err, db) {
-        if(err) throw err;
-        var collection = db.collection('polls');
-        collection.find({
-            url: '/polls/' + str
-        }).toArray(function(err, doc) {
-            obj.poll = doc[0];
-            res.render('results', obj);
-            db.close();
-        });
+
+    var collection = req.db.collection('polls');
+    collection.find({
+        url: '/polls/' + str
+    }).toArray(function(err, doc) {
+        obj.poll = doc[0];
+        res.render('results', obj);
     });
+
 });
 
-app.get('/polls/:STRING', function(req, res) {
-    var urlStr = req.params.STRING;
-    // check if user or ip has voted
-    // if yes, redirect to results
-    // else, redirect to vote page
-    res.redirect('/polls/' + urlStr + '/vote');
-});
-
-app.get('/data', function(req, res) {
+app.get('/data', passDb, function(req, res) {
     var query = req.query;
-    mongo.connect(process.env.MONGOLAB_URI, function(err, db) {
-    
-        var collection = db.collection('polls');
-        collection.find({
-            url: query.url
-        }).toArray(function(err, doc) {
-            res.json(doc);
-            db.close();
-        });
-    
+       
+    var collection = req.db.collection('polls');
+    collection.find({
+        url: query.url
+    }).toArray(function(err, doc) {
+        res.json(doc);
     });
+    
     
 });
 

74ff0354be47eae4389f1aa50154fd8a76e4a1d8
meek <otmeek@gmail.com>
2016-03-16 17:47:31 +0000

    restructured files in preparation for working with passport

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,57 +1,234 @@
 'use strict';
 
-var path = process.cwd();
-var ClickHandler = require(path + '/app/controllers/clickHandler.server.js');
+var randomstring = require('randomstring');
 
-module.exports = function (app, passport) {
+module.exports = function(app, passport, passDb) {     
 
-	function isLoggedIn (req, res, next) {
-		if (req.isAuthenticated()) {
-			return next();
-		} else {
-			res.redirect('/login');
-		}
-	}
+    app.get('/', passDb, function(req, res) {
 
-	var clickHandler = new ClickHandler();
+        var polls = {};
 
-	app.route('/')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/index.html');
-		});
+        var collection = req.db.collection('polls');
+        var data = collection.find().sort({
+            _id: -1
+        }).limit(50).toArray(function(err, docs) {
+            if(err) throw err;
+            polls.data = docs;
+            polls.currentPage = 'Home'
+            res.render('index', polls);
+        });
+    });
 
-	app.route('/login')
-		.get(function (req, res) {
-			res.sendFile(path + '/public/login.html');
-		});
+    app.get('/signin', function(req, res) {
+        var obj = {
+            currentPage: 'Signin'
+        }
+        res.render('signin', obj);
+    });
 
-	app.route('/logout')
-		.get(function (req, res) {
-			req.logout();
-			res.redirect('/login');
-		});
+    app.get('/signup', function(req, res) {
+        var obj = {
+            currentPage: 'Signup'
+        }
+        res.render('signup', obj);
+    });
 
-	app.route('/profile')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/profile.html');
-		});
+    app.get('/user/:USERNAME', function(req, res) {
+        // check that user is logged in?
+    });
 
-	app.route('/api/:id')
-		.get(isLoggedIn, function (req, res) {
-			res.json(req.user.github);
-		});
+    app.get('/polls/create', function(req, res) {
+        var obj = {
+            currentPage: 'Create'
+        }
+        res.render('create', obj);
+    });
 
-	app.route('/auth/github')
-		.get(passport.authenticate('github'));
+    app.post('/polls/create', passDb, function(req, res) {
+        var poll = req.body;
 
-	app.route('/auth/github/callback')
-		.get(passport.authenticate('github', {
-			successRedirect: '/',
-			failureRedirect: '/login'
-		}));
+        for (var p in poll) {
+          if (poll[p] === null || poll[p] === '') {
+            delete poll[p];
+          }
+        }
 
-	app.route('/api/:id/clicks')
-		.get(isLoggedIn, clickHandler.getClicks)
-		.post(isLoggedIn, clickHandler.addClick)
-		.delete(isLoggedIn, clickHandler.resetClicks);
-};
+        var doc = {
+            title: poll.title,
+            totalVotes: 0,
+            hasVoted: []
+        };
+        var choices = [];
+        for (var prop in poll) {
+            if (poll.hasOwnProperty(prop)) {
+                if (prop.substr(0, 6) == 'choice' && poll[prop] != '') {
+                    console.log(poll[prop])
+                    choices.push(poll[prop])
+                }
+            }
+        }
+
+        doc.choices = choices;
+
+        var collection = req.db.collection('polls');
+
+        function getNewUrl() {
+            // generate new URL
+            var urlStr = randomstring.generate(10);
+            // check if url already exists
+            collection.find({
+                url: '/polls/' + urlStr
+            }).toArray(function(err, docs) {
+                if(err) throw err;
+                if(docs.length > 0)
+                    getNewUrl();
+            });
+            return urlStr;
+        }
+
+        doc.url = '/polls/' + getNewUrl();
+
+        collection.insert(doc, function(err) {
+            if(err) throw err;
+            var obj = {
+                pollSubmitted: true,
+                poll: {
+                    url: process.env.APP_URL + doc.url.substr(1)
+                }
+            }
+            res.render('create', obj);
+        });
+
+
+    });
+
+    app.get('/polls/:STRING', function(req, res) {
+        var str = req.params.STRING;
+        res.redirect('/polls/' + str + '/vote');
+    });
+
+    app.get('/polls/:STRING/vote', passDb, function(req, res) {
+        var str = req.params.STRING;
+
+
+        var collection = req.db.collection('polls');
+
+
+        // if user has voted, redirect to results page
+        var ip = [req.headers['x-forwarded-for']];
+        collection.find({
+            url: '/polls/' + str,
+            hasVoted: {
+                $in: ip
+            }
+        }).toArray(function(err, docs) {
+            if(err) throw err;
+            if(docs.length>0) {
+                // redirect to results page
+                res.redirect('/polls/' + str + '/results');
+            }
+            else {
+                // user hasnt voted, render vote page
+                collection.find({
+                    url: /polls/ + str
+                }).toArray(function(err, docs) {
+                    if(err) throw err;
+                    var obj = {
+                        poll: docs[0]
+                    };
+                    res.render('poll', obj);
+                });
+            }
+        });
+
+    });
+
+    app.post('/polls/:STRING/vote', passDb, function(req, res) {
+        var str = req.params.STRING;
+        var vote = req.body;
+        var collection = req.db.collection('polls');
+        if(JSON.stringify(vote) === JSON.stringify({})) {
+            
+            collection.find({
+                    url: /polls/ + str
+                }).toArray(function(err, docs) {
+                    if(err) throw err;
+                    var obj = {
+                        poll: docs[0],
+                        voteFailed: true
+                    };
+                    res.render('poll', obj);
+                });
+            
+        }
+        else {
+
+            var key = vote.choice;
+            var voteObj = {};
+            voteObj[key] = 1;
+            voteObj.totalVotes = 1;
+            var pushObj = {
+                hasVoted: {
+                    $each: [req.headers['x-forwarded-for']]
+                }
+            }
+
+            if(vote.hasOwnProperty('newOption')) {
+                console.log(vote.hasOwnProperty('newOption'));
+                pushObj = {
+                    hasVoted: {
+                        $each: [req.headers['x-forwarded-for']]
+                    },
+                    choices: {
+                        $each: [vote.newOption]
+                    }
+                }
+            }
+
+            collection.update({
+                url: '/polls/' + str
+            }, {
+                $inc: voteObj,
+                $push: pushObj
+            }, function(err) {
+                if(err) throw err;
+                // redirect to results page
+                res.redirect('/polls/' + str + '/results');
+            });
+        }
+
+
+    });
+
+    app.get('/polls/:STRING/results', passDb, function(req, res) {
+        var str = req.params.STRING;
+        var obj = {};
+
+        var collection = req.db.collection('polls');
+        collection.find({
+            url: '/polls/' + str
+        }).toArray(function(err, doc) {
+            obj.poll = doc[0];
+            res.render('results', obj);
+        });
+
+    });
+
+    app.get('/data', passDb, function(req, res) {
+        var query = req.query;
+
+        var collection = req.db.collection('polls');
+        collection.find({
+            url: query.url
+        }).toArray(function(err, doc) {
+            res.json(doc);
+        });
+
+
+    });
+
+    app.get('/*', function(req, res) {
+        // redirect invalid paths to main page
+        res.redirect('/');
+    });
+}
--- server.js
+++ server.js
@@ -3,12 +3,43 @@
 var express      = require('express');
 var mongo        = require('mongodb').MongoClient;
 var path         = require('path');
-var randomstring = require('randomstring');
 var bodyParser   = require('body-parser');
 var os           = require('os');
 var ifaces       = os.networkInterfaces();
 
+var passport     = require('passport');
+var flash        = require('connect-flash');
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var session      = require('express-session');
+
+var configDB     = require('./config/database.js');
+
 var app = express();
+
+require('dotenv').load();
+
+app.use(express.static(__dirname + '/public'));
+app.use('/polls', express.static(__dirname + '/public'));
+app.use('/polls/:STRING', express.static(__dirname + '/public'));
+
+app.use(bodyParser.urlencoded({     
+    extended: false
+}));
+app.use(bodyParser.json())
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(session({
+    secret: 'everysinglecatiscute',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+app.set('view engine', 'jade');
+
 var db;
 
 var passDb = function(req, res, next) {
@@ -26,214 +57,9 @@ var passDb = function(req, res, next) {
 	}
 }
 
-require('dotenv').load();
-
-app.use(express.static(__dirname + '/public'));
-app.use('/polls', express.static(__dirname + '/public'));
-app.use('/polls/:STRING', express.static(__dirname + '/public'));
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-    extended: false
-}));
-app.use(bodyParser.json())
-app.set('view engine', 'jade');
-
-app.get('/', passDb, function(req, res) {
-    
-    var polls = {};
-    
-    var collection = req.db.collection('polls');
-    var data = collection.find().sort({
-        _id: -1
-    }).limit(50).toArray(function(err, docs) {
-        if(err) throw err;
-        polls.data = docs;
-        polls.currentPage = 'Home'
-        res.render('index', polls);
-    });
-});
-
-app.get('/polls/create', function(req, res) {
-    var obj = {
-        currentPage: 'Create'
-    }
-    res.render('create', obj);
-});
-
-app.post('/polls/create', passDb, function(req, res) {
-    var poll = req.body;
-
-    for (var p in poll) {
-      if (poll[p] === null || poll[p] === '') {
-        delete poll[p];
-      }
-    }
-    
-    var doc = {
-        title: poll.title,
-        totalVotes: 0,
-        hasVoted: []
-    };
-    var choices = [];
-    for (var prop in poll) {
-        if (poll.hasOwnProperty(prop)) {
-            if (prop.substr(0, 6) == 'choice' && poll[prop] != '') {
-                console.log(poll[prop])
-                choices.push(poll[prop])
-            }
-        }
-    }
-    
-    doc.choices = choices;
-
-    var collection = req.db.collection('polls');
-
-    function getNewUrl() {
-        // generate new URL
-        var urlStr = randomstring.generate(10);
-        // check if url already exists
-        collection.find({
-            url: '/polls/' + urlStr
-        }).toArray(function(err, docs) {
-            if(err) throw err;
-            if(docs.length > 0)
-                getNewUrl();
-        });
-        return urlStr;
-    }
-
-    doc.url = '/polls/' + getNewUrl();
-
-    collection.insert(doc, function(err) {
-        if(err) throw err;
-        var obj = {
-            pollSubmitted: true,
-            poll: {
-                url: process.env.APP_URL + doc.url.substr(1)
-            }
-        }
-        res.render('create', obj);
-    });
-    
-    
-});
-
-app.get('/polls/:STRING/vote', passDb, function(req, res) {
-    var str = req.params.STRING;
-    
-
-    var collection = req.db.collection('polls');
-        
-        
-    // if user has voted, redirect to results page
-    var ip = [req.headers['x-forwarded-for']];
-    collection.find({
-        url: '/polls/' + str,
-        hasVoted: {
-            $in: ip
-        }
-    }).toArray(function(err, docs) {
-        if(err) throw err;
-        if(docs.length>0) {
-            // redirect to results page
-            res.redirect('/polls/' + str + '/results');
-        }
-        else {
-            // user hasnt voted, render vote page
-            collection.find({
-                url: /polls/ + str
-            }).toArray(function(err, docs) {
-                if(err) throw err;
-                var obj = {
-                    poll: docs[0]
-                };
-                res.render('poll', obj);
-            });
-        }
-    });
-
-});
-
-app.post('/polls/:STRING/vote', passDb, function(req, res) {
-    var str = req.params.STRING;
-    var vote = req.body;
-    console.log(vote);
-    
-    var key = vote.choice;
-    var voteObj = {};
-    voteObj[key] = 1;
-    voteObj.totalVotes = 1;
-    var pushObj = {
-        hasVoted: {
-            $each: [req.headers['x-forwarded-for']]
-        }
-    }
-    
-    if(vote.hasOwnProperty('newOption')) {
-        console.log(vote.hasOwnProperty('newOption'));
-        pushObj = {
-            hasVoted: {
-                $each: [req.headers['x-forwarded-for']]
-            },
-            choices: {
-                $each: [vote.newOption]
-            }
-        }
-    }
-    
-    var collection = req.db.collection('polls');
-    collection.update({
-        url: '/polls/' + str
-    }, {
-        $inc: voteObj,
-        $push: pushObj
-    }, function(err) {
-        if(err) throw err;
-        // redirect to results page
-        res.redirect('/polls/' + str + '/results');
-    });
-
-    
-});
-
-app.get('/polls/:STRING/results', passDb, function(req, res) {
-    var str = req.params.STRING;
-    var obj = {};
-
-    var collection = req.db.collection('polls');
-    collection.find({
-        url: '/polls/' + str
-    }).toArray(function(err, doc) {
-        obj.poll = doc[0];
-        res.render('results', obj);
-    });
-
-});
-
-app.get('/data', passDb, function(req, res) {
-    var query = req.query;
-       
-    var collection = req.db.collection('polls');
-    collection.find({
-        url: query.url
-    }).toArray(function(err, doc) {
-        res.json(doc);
-    });
-    
-    
-});
-
-app.get('/*', function(req, res) {
-    // redirect invalid paths to main page
-    res.redirect('/');
-});
+require('./app/routes/index.js')(app, passport, passDb);
 
 var port = process.env.PORT || 8080;
 app.listen(port,  function () {
 	console.log('Node.js listening on port ' + port + '...');
 });
-
-// to do
-// error handling
-// login
-// db
-// poll

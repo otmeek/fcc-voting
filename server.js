1fb231cf567f1bbb19b8f4589953512fed879569
meek <otmeek@gmail.com>
2016-03-16 15:07:34 +0000

    Revert "get started on authentication"
    This reverts commit ad0faa7cccf1f16481697d412801d656b3cd1058.

--- server.js
+++ server.js
@@ -7,10 +7,6 @@ var randomstring = require('randomstring');
 var bodyParser   = require('body-parser');
 var os           = require('os');
 var ifaces       = os.networkInterfaces();
-var passport     = require('passport');
-var GitHubStrategy = require('passport-github').Strategy;
-var cookieParser = require('cookie-parser');
-var session = require('express-session')
 
 var app = express();
 var db;
@@ -40,49 +36,6 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 }));
 app.use(bodyParser.json())
 app.set('view engine', 'jade');
-app.use(session({
-  secret: 'keyboard cat'
-}))
-app.use(passport.initialize());
-app.use(cookieParser());
-app.use(passport.session());
-passport.serializeUser(function(user, done) {
-  done(null, user.id);
-});
-
-passport.deserializeUser(function(id, done) {
-    var users = db.collection('users');
-  users.find({
-        id: id
-    }).toArray(function(err, docs) {
-      done(err, docs[0]);
-  });
-});
-passport.use(new GitHubStrategy({
-    clientID: process.env.GITHUB_CLIENT_ID,
-    clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: "http://localhost:8080/auth/github/callback"
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    var users = db.collection('users');
-    
-    // find user
-    users.find({
-        id: profile.id
-    }).toArray(function(err, docs) {
-        if(docs.length===0) {
-            users.insert({
-                id: profile.id
-            }, function(err, record) {
-                return cb(err, record[0]);
-            });
-        }
-        else {
-            return cb(err, docs[0]);
-        }
-    })
-  }
-));
 
 app.get('/', passDb, function(req, res) {
     
@@ -99,24 +52,10 @@ app.get('/', passDb, function(req, res) {
     });
 });
 
-app.get('/auth/github', passport.authenticate('github'));
-
-app.get('/auth/github/callback', passport.authenticate('github', {
-    failureRedirect: '/login'
-}), function(req, res) {
-    res.redirect('/');
-});
-
-app.get('/signin', function(req, res) {
-    res.render('login');
-})
-
 app.get('/polls/create', function(req, res) {
     var obj = {
         currentPage: 'Create'
     }
-    
-    console.log(req.user);
     res.render('create', obj);
 });
 
@@ -217,7 +156,6 @@ app.get('/polls/:STRING/vote', passDb, function(req, res) {
 app.post('/polls/:STRING/vote', passDb, function(req, res) {
     var str = req.params.STRING;
     var vote = req.body;
-    var ip = req.headers['x-forwarded-for'];
     console.log(vote);
     
     var key = vote.choice;
@@ -234,7 +172,7 @@ app.post('/polls/:STRING/vote', passDb, function(req, res) {
         console.log(vote.hasOwnProperty('newOption'));
         pushObj = {
             hasVoted: {
-                $each: [ip]
+                $each: [req.headers['x-forwarded-for']]
             },
             choices: {
                 $each: [vote.newOption]
@@ -243,31 +181,15 @@ app.post('/polls/:STRING/vote', passDb, function(req, res) {
     }
     
     var collection = req.db.collection('polls');
-    
-    collection.find({
-        url: '/polls/' + str,
-        hasVoted: {
-            $in: [ip]
-        }
-    }).toArray(function(err, docs) {
+    collection.update({
+        url: '/polls/' + str
+    }, {
+        $inc: voteObj,
+        $push: pushObj
+    }, function(err) {
         if(err) throw err;
-        if(docs.length>0) {
-            // redirect to results page
-            res.redirect('/polls/' + str + '/results');
-        }
-        else {
-            collection.update({
-                url: '/polls/' + str
-            }, {
-                $inc: voteObj,
-                $push: pushObj
-            }, function(err) {
-                if(err) throw err;
-                // redirect to results page
-                res.redirect('/polls/' + str + '/results');
-            });
-        }
-
+        // redirect to results page
+        res.redirect('/polls/' + str + '/results');
     });
 
     
